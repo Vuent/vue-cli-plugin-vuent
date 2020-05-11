e698826d68cc1af413e84b97b41ee21bbd03ce97
Artur Mizera <arturmiz@users.noreply.github.com>
2020-05-11 23:00:43 +0200

    feat: bumped up to use v0.7.0

--- generator.js
+++ generator.js
@@ -1,9 +1,9 @@
 module.exports = (api, options, rootOptions) => {
   api.extendPackage({
     dependencies: {
-      'vuent': '^0.4.0'
+      'vuent': '^0.7.0'
     }
   })
 
-  api.injectImports(api.entryFile, `import Vuent from 'vuent'`)
+  api.injectImports(api.entryFile, `import Vuent from 'vuent';`)
 }
